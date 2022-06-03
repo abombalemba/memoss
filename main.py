b9bbbb48fb38b30468357125ba8a98ccc430cc1a
syetnoy <83402627+syetnoy@users.noreply.github.com>
2022-06-04 01:18:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, flash, redirect, request, send_file, jsonify
 from werkzeug.utils import secure_filename
-import flask
 
 import sqlite3
+from json import loads, dumps
 from random import randint, choice
-from os import environ
+from os import environ, path
 from PIL import Image
 from datetime import datetime
 
@@ -12,9 +12,10 @@ from datetime import datetime
 server = Flask(__name__)
 server.config['SECRET_KEY'] = '0a17e0af1f364c72065534d68a916153a780a5fc'
 server.config['UPLOAD_FOLDER'] = './static/content/'
-server.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+server.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 4
 
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif', 'tiff']
+COUNT_POSTS = 20
 
 connect, cursor = None, None
 
@@ -22,7 +23,7 @@ connect, cursor = None, None
 @server.route('/')
 @server.route('/gallery')
 def index():
-    return render_template('main.html', left_column=column(5), center_column=column(5), right_column=column(5))
+    return render_template('index.html', left_column=column(COUNT_POSTS), center_column=column(COUNT_POSTS), right_column=column(COUNT_POSTS))
 
 
 @server.route('/reg')
@@ -68,11 +69,16 @@ def add():
     return render_template('add.html')
 
 
-@server.route('/api/<string:link>')
-def api(link):
-    link = link.split('/')
-    if link[0] == 'login':
-        params = create_response(link[1])
+@server.route('/chat/<string:params>')
+def chat(params):
+    params = create_response(params)
+
+
+@server.route('/api/<string:link>/<string:params>', methods=['GET', 'POST'])
+def api(link, params):
+    print(link, params)
+    if link == 'login':
+        params = create_response(params)
         response = database_query(f"""SELECT * FROM `accounts` WHERE `Nickname` = "{params['login']}" """)
         if response[0]:
             if response[0][3] == params['password']:
@@ -83,8 +89,8 @@ def api(link):
         else:
             return render_template('log.html', message='Аккаунт не найден!')
 
-    if link[1] == 'reg':
-        params = create_response(link[1])
+    if link == 'reg':
+        params = create_response(params)
         if not params['nickname']:
             return render_template('reg.html', message='Введите ник')
         if not params['email']:
@@ -103,39 +109,59 @@ def api(link):
 
         return render_template('main.html', message='Аккаунт создан')
 
-    if link[1] == 'like':
-        params = create_response(link[0])
-        database_query(f"""UPDATE `accounts` SET `Likes` = `Likes` + 1 WHERE `ID` = {params['IDpost']}""")
+    if link == 'like':
+        params = create_response(params)
+        print(params)
+        database_query(f"""UPDATE `content` SET `Likes` = `Likes` + 1 WHERE `ID` = {params['IDpost']}""")
 
+    if link == 'dislike':
+        params = create_response(params)
+        database_query(f"""UPDATE `content` SET `Dislikes` = `Dislikes` + 1 WHERE `ID` = {params['IDpost']}""")
 
-@server.route('/send_file/<string:filename>')
-def send_file(filename):
-    path = './static/content/' + filename
-    print(path, filename)
-    return flask.send_file(path, as_attachment=True)
+    if link == 'comment':
+        params = create_response(params)
+        database_query(f"""INSERT INTO `comments`" (`IDpost`, `IDuser`, `Text`, `Date`, `Time`) VALUES """
+                       f"""({params['IDpost']}, {params['IDuser']}, "{params['Text']}", "{get_date()}", "{get_time()}")""")
 
+    if link == 'send_file':
+        params = create_response(params)
+        return send_file('./static/content/' + params['filename'], as_attachment=True)
 
-@server.route('/get_file/', methods=['GET', 'POST'])
-def get_file():
-    if request.method == 'GET':
-        return render_template('add.html')
+    if link == 'get_file':
+        params = create_response(params)
+        if request.method == 'GET':
+            return render_template('add.html')
 
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            r = file.read()
-            with open(f'./static/content/{filename}', mode='wb') as f:
-                f.write(r)
-    return render_template('add.html')
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                r = file.read()
+                with open(f'./static/content/{filename}', mode='wb') as f:
+                    f.write(r)
+
+                sizes = get_file_sizes(f'./static/content/{filename}')
+                weight = get_file_weight(f'static/content/{filename}')
+                print(sizes, weight)
+                database_query(f'INSERT INTO `content` (`Name`, `Description`, `URL`, `Date`, `Time`, `Width`, `Height`) VALUES'
+                               f'("{filename}", "{1}", "{get_URL(16)}", "{get_date()}", "{get_time()}", {sizes[0]}, {sizes[1]})')
+
+    if link == 'send_message':
+        params = create_response(params)
+        database_query(f"""INSERT INTO `chat` (`IDuser`, `Text`, `Date`, `Time`) VALUES ({params['IDuser']}, "{params['Text']}", "{get_date()}", "{get_time()}")""")
+
+    if link == 'get_message':
+        result = database_query(f"""SELECT * FROM `chat` ORDER BY `ID` DESC LIMIT 50""")
+        return jsonify(result)
+    return dumps({'a': 123})
 
 
 def error():
@@ -157,24 +183,24 @@ def database_query(query=''):
 
 
 def create_dict_post(result, comments):
-    result = {
+    return {
         'ID': result[0],
         'Name': result[1],
         'Description': result[2],
         'Views': result[3],
         'Likes': result[4],
+        'Dislikes': result[5],
         'Comments': comments,
-        'URL': result[5],
-        'Date': result[6],
-        'Time': result[7],
-        'Width': result[8],
-        'Height': result[9],
+        'URL': result[6],
+        'Date': result[7],
+        'Time': result[8],
+        'Width': result[9],
+        'Height': result[10],
     }
-    return result
 
 
 def create_dict_comments(result):
-    result = {
+    return {
         'ID': result[0],
         'IDpost': result[1],
         'IDuser': result[2],
@@ -182,11 +208,10 @@ def create_dict_comments(result):
         'Date': result[4],
         'Time': result[5],
     }
-    return result
 
 
 def create_dict_user(result):
-    result = {
+    return {
         'ID': result[0],
         'Nickname': result[1],
         'Email': result[2],
@@ -195,14 +220,22 @@ def create_dict_user(result):
         'Preview': result[5],
         'LikedPosts': result[6],
     }
-    return result
+
+
+def create_dict_message(result):
+    return {
+        'ID': result[0],
+        'IDuser': result[1],
+        'Text': result[2],
+        'Date': result[3],
+        'Time': result[4]
+    }
 
 
 def column(n=10):
     posts = []
     for j in range(n):
         result = database_query(f'SELECT * FROM `content` WHERE ID = {randint(1, database_query("SELECT COUNT(*) FROM `content`")[0][0])}')
-        print(result)
         comments = database_query(f'SELECT COUNT(`ID`) FROM `comments` WHERE `IDpost` = {result[0][0]}')
         database_query(f'UPDATE `content` SET `Views` = `Views` + 1 WHERE `ID` = {result[0][0]}')
         result = create_dict_post(result[0], comments[0][0])
@@ -233,6 +266,10 @@ def get_time():
 
 def get_file_sizes(file_name):
     return Image.open(file_name).sizes
+
+
+def get_file_weight(file_name):
+    return path.getsize(file_name)
 
 
 def get_URL(n):
